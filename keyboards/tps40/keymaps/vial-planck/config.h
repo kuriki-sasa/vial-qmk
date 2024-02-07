5a670fd4c52acf7f00a523b33c7bfc453c88f5c4
kuriki-sasa <sasaki.rick@icloud.com>
2024-02-07 20:37:21 +0900

    Rename vial to vial-planck

---
+++ keyboards/tps40/keymaps/vial-planck/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCF, 0x76, 0x10, 0x03, 0x7F, 0x3E, 0x83, 0xD0}
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
--- keyboards/tps40/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x6C, 0xD3, 0xB6, 0xE3, 0xFA, 0x22, 0x3F, 0x26}
-#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 2, 11 }
