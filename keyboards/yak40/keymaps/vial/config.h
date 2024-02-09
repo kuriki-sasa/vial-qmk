56def703659cc940026b74d5bf9e8a9022140666
kuriki-sasa <sasaki.rick@icloud.com>
2024-02-09 23:11:27 +0900

    Add Yak40 keyboard

---
+++ keyboards/yak40/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEC, 0x6E, 0x52, 0xD1, 0xBE, 0x5F, 0x29, 0x82}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
