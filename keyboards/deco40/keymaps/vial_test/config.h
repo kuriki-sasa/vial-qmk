1432e30ad972c8863ce6a779a7b77dc2e47c8c3d
kuriki-sasa <sasaki.rick@icloud.com>
2025-03-22 21:20:10 +0900

    Add deco40

---
+++ keyboards/deco40/keymaps/vial_test/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBA, 0xDE, 0xDA, 0x5D, 0x8C, 0x30, 0x27, 0xAB}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
